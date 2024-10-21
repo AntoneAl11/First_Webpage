403a8d255fd7da806a103d47b813aab577d55a5c
Antonio Aleman <aleman.antonio.95@gmail.com>
2024-10-21 00:16:46 -0600

    Calculos de porcentajes de población

--- index.js
+++ index.js
@@ -30,3 +30,27 @@ tmec.push({
 
 //Imprimir valores para verificar funcionalidad
 console.log(tmec);
+
+// Declarar las poblaciones
+let pobCan = tmec[2].Poblacion;
+let pobUS = tmec[0].Poblacion;
+let pobMex = tmec[1].Poblacion;
+
+//Calcular el total de la población
+let totalPob = pobCan + pobUS + pobMex;
+
+//Calcular los porcentajes 
+let porcentajeMexEnNA = pobMex / totalPob;
+let porcentajeMexVsUS = pobMex / pobUS;
+let porcentajeMexVsCan = pobMex / pobCan;
+let porcentajeUSVsCan = pobUS / pobCan;
+let porcentajeCanVsUSMex = pobCan / (pobUS + pobMex);
+
+//Imprimir los resultados
+console.log(`Total de población: ${totalPob}`);
+console.log(`Porcentaje de Mexicanos en América del Norte: ${porcentajeMexEnNA}`);
+console.log(`Porcentaje de México contra la población de Estados Unidos: ${porcentajeMexVsUS}%`);
+console.log(`Porcentaje de México contra la población de Canadá: ${porcentajeMexVsCan}%`);
+console.log(`Porcentaje de Estados Unidos contra la población de Canadá: ${porcentajeUSVsCan}%`);
+console.log(`Porcentaje de Canadá contra la población de Estados Unidos y México: ${porcentajeCanVsUSMex}%`);
+
